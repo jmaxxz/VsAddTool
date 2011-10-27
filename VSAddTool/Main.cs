1683d05e85e5491f7c2322dce74647d138188cf4
Jmaxxz <jmaxxz@Jmaxxz-Linux.(none)>
2011-10-26 23:41:21 -0500

    Fix whitespace to use spaces.

--- VSAddTool/Main.cs
+++ VSAddTool/Main.cs
@@ -3,35 +3,35 @@ using Microsoft.Win32;
 
 namespace VSAddTool
 {
-	class MainClass
-	{
-		public static int Main (string[] args)
-		{
-			string command = "";
-			string parameters = "";
-		    string name = "";
-		    string dir = "";
-		    int options = 18;
+    class MainClass
+    {
+        public static int Main (string[] args)
+        {
+            string command = "";
+            string parameters = "";
+            string name = "";
+            string dir = "";
+            int options = 18;
 			
-			Options opts = new Options
+            Options opts = new Options
 			{
                 new Option(new string[]{}, x=>name=x),
 				new Option(new string[]{}, x=>command=x),
 				new Option(new []{"p","params"}, x=>parameters=x),
                 new Option(new []{"d","startdir"}, x=>dir=x),
 			};
-		    opts.Parse(args);
+            opts.Parse (args);
 			
-			if(command =="" || name == "")
-			{
-                Console.WriteLine("Usage: VSAddTool.exe <Tool Name> <Command> [-p <command parameters>][-d <start in directory>]");
-				return 1;	
-			}
+            if (command == "" || name == "")
+            {
+                Console.WriteLine ("Usage: VSAddTool.exe <Tool Name> <Command> [-p <command parameters>][-d <start in directory>]");
+                return 1;	
+            }
 
 
             using (var exToolKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\10.0\External Tools",true))
             {
-                int numOfTools = (int) exToolKey.GetValue("ToolNumKeys");
+                int numOfTools = (int)exToolKey.GetValue ("ToolNumKeys");
 
                 string pathVal = "ToolCmd" + numOfTools;
                 string argsVal = "ToolArg" + numOfTools;
@@ -41,17 +41,17 @@ namespace VSAddTool
 
 
 
-                exToolKey.SetValue(pathVal, command, RegistryValueKind.String);
-                exToolKey.SetValue(argsVal, parameters, RegistryValueKind.String);
-                exToolKey.SetValue(nameVal, name, RegistryValueKind.String);
-                exToolKey.SetValue(dirVal, dir, RegistryValueKind.String);
-                exToolKey.SetValue(optsVal, options, RegistryValueKind.DWord);
-                exToolKey.SetValue("ToolNumKeys", numOfTools + 1, RegistryValueKind.DWord);
+                exToolKey.SetValue (pathVal, command, RegistryValueKind.String);
+                exToolKey.SetValue (argsVal, parameters, RegistryValueKind.String);
+                exToolKey.SetValue (nameVal, name, RegistryValueKind.String);
+                exToolKey.SetValue (dirVal, dir, RegistryValueKind.String);
+                exToolKey.SetValue (optsVal, options, RegistryValueKind.DWord);
+                exToolKey.SetValue ("ToolNumKeys", numOfTools + 1, RegistryValueKind.DWord);
             }
 
 
 
-		    return 0;
-		}
-	}
+            return 0;
+        }
+    }
 }
